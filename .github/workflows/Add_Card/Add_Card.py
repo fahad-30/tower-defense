5f881f5ff834acb2f58347a4cc36f58dbca9b06c
Fahad Sheikh <fahadsheikh3010@gmail.com>
2020-04-21 09:34:43 +0530

    Updating the python file

--- .github/workflows/Add_Card/Add_Card.py
+++ .github/workflows/Add_Card/Add_Card.py
@@ -9,7 +9,7 @@ query = {
 
 'name':name,
 'desc':desc,
-'idList':'5e8f29de5d92c804e5461b1a',
+'idList':'5e95c3a80d601f5535eb4259',
 'key':'2227ef7694cce98f830eb80a7d545ef7',
 'token':'03341061d27ae05bb27b3e1c82beffb7b1472bd580c9b0631401773f2f007c80'
 }
